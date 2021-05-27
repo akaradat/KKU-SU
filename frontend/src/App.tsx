b04a17d4bfd490167b062637418652132493e185
dreamnajababy <patnarin_s@kkumail.com>
2021-05-27 18:14:34 +0700

    [Frontend] update coding style to TSX and test scss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(): JSX.Element {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            อิหยังวะหนิ ฮัลโหลลลวัดตตาฟัค Edit <code>src/App.tsx</code> and save
+            to jing a paw.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
