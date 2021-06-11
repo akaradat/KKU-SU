e15ec035fee5a5b682142530a05226ca375fcb9d
Akrd kmnc <akrd.kmnc@gmail.com>
2021-06-11 17:37:39 +0700

    chore: remove eslint disable in misc

--- frontend/src/components/misc/Layouts.tsx
+++ frontend/src/components/misc/Layouts.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable */
-import tw from "twin.macro";
+import tw from 'twin.macro';
 
 export const Container = tw.div`relative`;
 export const ContentWithPaddingXl = tw.div`max-w-screen-xl mx-auto py-20 lg:py-24`;
