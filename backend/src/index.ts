b0cd9c3e8d9abfbb0d41c0e82e001960fbfa8581
dreamnajababy <patnarin_s@kkumail.com>
2021-06-12 18:10:27 +0700

    init express structure

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,19 @@
-const HelloWorld = () => "Hello Mother Fucker." 
-export default HelloWorld
+import * as http from 'http';
+import app from './app'
+import debug from 'debug';
+import {CommonRoutesConfig} from './common/common.routes.config';
+import {UsersRoutes} from './users/users.routes.config';
+
+const routes: Array<CommonRoutesConfig> = [];
+const debugLog: debug.IDebugger = debug('app');
+const port = 3000;
+const server: http.Server = http.createServer(app);
+routes.push(new UsersRoutes(app));
+
+server.listen(port, () => {
+	console.log(`We're running at ${port}`)
+	debugLog(`Server running at http://localhost:${port}`);
+	routes.forEach((route: CommonRoutesConfig) => {
+	    debugLog(`Routes configured for ${route.getName()}`);
+	});
+});
