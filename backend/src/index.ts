85829151b3435b0a640c006b1a1cc79bbdeb6f45
dreamnajababy <patnarin_s@kkumail.com>
2021-06-12 19:20:07 +0700

    compose services together

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import * as http from 'http';
+import * as dotenv from 'dotenv'
 import app from './app'
 import debug from 'debug';
 import {CommonRoutesConfig} from './common/common.routes.config';
@@ -6,12 +7,13 @@ import {UsersRoutes} from './users/users.routes.config';
 
 const routes: Array<CommonRoutesConfig> = [];
 const debugLog: debug.IDebugger = debug('app');
-const port = 3000;
+const port = 8000;
 const server: http.Server = http.createServer(app);
+dotenv.config()
 routes.push(new UsersRoutes(app));
 
 server.listen(port, () => {
-	console.log(`We're running at ${port}`)
+	console.log(`We're running at ${port}, process:${process.env.NODE_ENV}`)
 	debugLog(`Server running at http://localhost:${port}`);
 	routes.forEach((route: CommonRoutesConfig) => {
 	    debugLog(`Routes configured for ${route.getName()}`);
