88236824368f42f1f9ded62b7df31109b091f9bc
Akrd kmnc <akrd.kmnc@kkumail.com>
2021-06-19 20:17:09 +0700

    chore: update users dto

--- api/src/dtos/users.dto.ts
+++ api/src/dtos/users.dto.ts
@@ -1,9 +1,32 @@
-import { IsString, IsEmail } from 'class-validator';
+import { IsString, IsEmail, Length, IsOptional } from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
   public email: string;
 
   @IsString()
+  @IsOptional()
   public password: string;
+
+  @IsString()
+  public name: string;
+
+  @IsString()
+  public nickname: string;
+
+  @IsString()
+  @Length(11, 11)
+  public studentId: string;
+
+  @IsString()
+  public faculty: string;
+
+  @IsString()
+  public branch: string;
+
+  @IsString()
+  public tel: string;
+
+  @IsString()
+  public facebook: string;
 }
