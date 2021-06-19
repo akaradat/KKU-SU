4ae2fad790d93d7efa6ea5c7dd284a3caebcae71
Akrd kmnc <akrd.kmnc@kkumail.com>
2021-06-19 19:35:51 +0700

    initial api backend

---
+++ api/src/interfaces/users.interface.ts
@@ -0,0 +1,12 @@
+export interface User {
+  id: number;
+  name: string;
+  nickname: string;
+  studentId: string;
+  faculty: string;
+  branch: string;
+  tel: string;
+  email: string;
+  facebook: string;
+  password: string;
+}
